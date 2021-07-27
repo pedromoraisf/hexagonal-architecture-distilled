ee11754f249a18d32cd74dae01a800ec0d8c1198
pedromoraisf <pedro.morais1997@gmail.com>
2021-07-27 13:48:27 -0300

    feat: add http framework mock adapter

---
+++ src/adapters/http/mock/http-framework.js
@@ -0,0 +1,10 @@
+const HttpFrameworkMockAdapter = () => ({
+  serverError: (message = "Internal server error") => ({
+    statusCode: 500,
+    body: {
+      message,
+    },
+  }),
+});
+
+module.exports = HttpFrameworkMockAdapter;
