527e29ce212e00f2ae0c7c867d35390cc050db6b
pedromoraisf <pedro.morais1997@gmail.com>
2021-07-26 21:11:27 -0300

    refactor: extract method

--- src/usecases/create-an-post/create-an-post.js
+++ src/usecases/create-an-post/create-an-post.js
@@ -1,6 +1,10 @@
 const { Post } = require("@/entities");
 
 const createAnPostUseCase = async ({ payload = Post }) => {
+  validateReceivedPublication(payload);
+};
+
+const validateReceivedPublication = (payload) => {
   const payloadValuesToArray = Object.values(payload);
   const hasInvalidPayloadProp = payloadValuesToArray.some(
     (prop) => typeof prop !== "string"
