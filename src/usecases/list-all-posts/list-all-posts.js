f45ab4fd0056aa660520520ef53ea43d385c59b2
pedromoraisf <pedro.morais1997@gmail.com>
2021-07-28 10:02:13 -0300

    feat: return an serverError if repository throws any low-level error

--- src/usecases/list-all-posts/list-all-posts.js
+++ src/usecases/list-all-posts/list-all-posts.js
@@ -2,7 +2,11 @@ const { PostRepositoryPort } = require("@/ports/database");
 const { HttpFrameworkPort } = require("@/ports/http-framework");
 
 const listAllPostsUseCase = async (postRepository = PostRepositoryPort, { ok, serverError } = HttpFrameworkPort) => {
-  const persistedPosts = await postRepository.listAll();
+  try {
+    const persistedPosts = await postRepository.listAll();
+  } catch {
+    return serverError();
+  }
 };
 
 module.exports = listAllPostsUseCase;
