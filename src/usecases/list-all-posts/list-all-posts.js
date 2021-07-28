ee8cbb219bcd4e9617319ff1a2c07ebe16352526
pedromoraisf <pedro.morais1997@gmail.com>
2021-07-28 10:03:44 -0300

    feat: return correctly ok response with persisted posts

--- src/usecases/list-all-posts/list-all-posts.js
+++ src/usecases/list-all-posts/list-all-posts.js
@@ -4,6 +4,7 @@ const { HttpFrameworkPort } = require("@/ports/http-framework");
 const listAllPostsUseCase = async (postRepository = PostRepositoryPort, { ok, serverError } = HttpFrameworkPort) => {
   try {
     const persistedPosts = await postRepository.listAll();
+    return ok(persistedPosts);
   } catch {
     return serverError();
   }
