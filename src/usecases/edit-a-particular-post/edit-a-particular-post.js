e3cf0e05d5f26d739bbb52394b9edac699e71ae1
pedromoraisf <pedro.morais1997@gmail.com>
2021-08-05 22:41:32 -0300

    feat: return an badRequest if repository not find the requested post

--- src/usecases/edit-a-particular-post/edit-a-particular-post.js
+++ src/usecases/edit-a-particular-post/edit-a-particular-post.js
@@ -3,13 +3,19 @@ const { HttpFrameworkPort } = require("@/ports/http-framework");
 const { validateReceivedPublication } = require("../helpers");
 const { editAParticularPostDTO } = require("./dto");
 
+const ExceptionEnums = {
+  NOT_FOUND: 404
+};
+
 const editAParticularPostUseCase = async (
   { payload = editAParticularPostDTO },
   postRepository = PostRepositoryPort,
-  { ok, serverError } = HttpFrameworkPort
+  { badRequest } = HttpFrameworkPort
 ) => {
   validateReceivedPublication(payload.data);
+
   const postHasUpdated = await postRepository.update(payload);
+  if (!postHasUpdated) return badRequest("The publication was not found");
 };
 
 module.exports = editAParticularPostUseCase;
