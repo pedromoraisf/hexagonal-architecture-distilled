27f419a2c7856c1bf8d8c161328d616d3ce0a484
pedromoraisf <pedro.morais1997@gmail.com>
2021-08-05 22:02:24 -0300

    test: ensure validate all required properties to update an post

---
+++ src/usecases/edit-a-particular-post/edit-a-particular-post.spec.js
@@ -0,0 +1,41 @@
+const editAParticularPostUseCase = require("./edit-a-particular-post");
+const { PostRepositoryInMemoryAdapter } = require("@/adapters/database/in-memory");
+const { HttpFrameworkMockAdapter } = require("@/adapters/http/mock");
+
+const makeFixture = () => ({
+  id: "any_id",
+  data: {
+    title: "any_updated_title",
+    content: "any_updated_content"
+  }
+});
+
+const makeSut = () => {
+  const makedPostRepositoryInMemoryAdapter = PostRepositoryInMemoryAdapter();
+  const makedHttpFrameworkMockAdapter = HttpFrameworkMockAdapter();
+  const sut = (payload) =>
+    editAParticularPostUseCase({ payload }, makedPostRepositoryInMemoryAdapter, makedHttpFrameworkMockAdapter);
+
+  return {
+    sut,
+    makedPostRepositoryInMemoryAdapter
+  };
+};
+
+describe("Edit a particular post", () => {
+  test("should validate all required properties to update an post", async () => {
+    const { sut } = makeSut();
+
+    const wrongFixture = {
+      ...makeFixture(),
+      data: {
+        ...makeFixture().data,
+        content: null
+      }
+    };
+
+    const testable = async () => await sut(wrongFixture);
+
+    await expect(testable).rejects.toThrow(new Error("Received publication to be wrong"));
+  });
+});
