17329c06cdf28ebbb122207e9873b3ef4e6030a6
pedromoraisf <pedro.morais1997@gmail.com>
2021-08-07 10:53:22 -0300

    feat: add handle error decorator

---
+++ src/usecases/decorators/handle-error.js
@@ -0,0 +1,16 @@
+const { HttpFrameworkPort } = require("@/ports/http-framework");
+const { Errors } = require("@/shared/error");
+
+const handleErrorDecorator = async (cb, { badRequest, serverError } = HttpFrameworkPort) => {
+  try {
+    return await cb();
+  } catch ({ message: error }) {
+    const parsedError = JSON.parse(error);
+    return {
+      [Errors.BAD_REQUEST]: badRequest(parsedError.payload),
+      [Errors.SERVER_ERROR]: serverError(parsedError.payload)
+    }[parsedError.type];
+  }
+};
+
+module.exports = handleErrorDecorator;
