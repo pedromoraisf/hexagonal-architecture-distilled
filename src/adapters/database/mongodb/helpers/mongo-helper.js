db23bca24ce758604284764d875e7f1d55811f77
pedromoraisf <pedro.morais1997@gmail.com>
2021-08-08 21:10:36 -0300

    feat: add mongo helper

---
+++ src/adapters/database/mongodb/helpers/mongo-helper.js
@@ -0,0 +1,23 @@
+const { MongoClient } = require("mongodb");
+
+const mongoHelper = () => ({
+  client: null,
+  url: null,
+  async connect(url) {
+    this.url = url;
+    this.client = await MongoClient.connect(url, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    });
+  },
+  async disconnect() {
+    await this.client.close();
+    this.client = null;
+  },
+  async getCollection(name) {
+    if (!this.client?.topology?.isConnected()) await this.connect(this.url);
+    return this.client.db().collection(name);
+  }
+});
+
+module.exports = mongoHelper;
