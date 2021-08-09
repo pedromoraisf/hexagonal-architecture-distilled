739a3a9d73e90af0eb95dc0db4cf4e8c3aec2e1a
pedromoraisf <pedro.morais1997@gmail.com>
2021-08-08 22:07:57 -0300

    chore: add createPostAndReturnId in driver to test sut

--- src/adapters/database/mongodb/post-repository/driver.js
+++ src/adapters/database/mongodb/post-repository/driver.js
@@ -1,6 +1,14 @@
+const { nanoid } = require("nanoid");
+
 const sutDriver = (sut) => ({
   async clearDatabase() {
     return sut.deleteMany({});
+  },
+  async createPostAndReturnId(fixture = {}) {
+    return sut.insertOne({
+      ...fixture,
+      _id: nanoid()
+    });
   }
 });
 
