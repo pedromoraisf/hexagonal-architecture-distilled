4b923da8406be379abc957f55f530122e8c6ad94
pedromoraisf <pedro.morais1997@gmail.com>
2021-08-13 22:03:31 -0300

    style: add glossary

--- src/usecases/index.js
+++ src/usecases/index.js
@@ -1,5 +1,6 @@
 const { listAllPostsUseCase } = require("./list-all-posts");
-const { createAPostUseCase } = require("./create-an-post");
+const { createAPostUseCase } = require("./create-a-post");
 const { editAParticularPostUseCase } = require("./edit-a-particular-post");
+const { getSpecificPostUseCase } = require("./get-specific-post");
 
-module.exports = { listAllPostsUseCase, createAPostUseCase, editAParticularPostUseCase };
+module.exports = { listAllPostsUseCase, createAPostUseCase, editAParticularPostUseCase, getSpecificPostUseCase };
