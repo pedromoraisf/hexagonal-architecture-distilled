04f6c4c64f9cb597122b7c732c2aa99d23d7c3b6
pedromoraisf <pedro.morais1997@gmail.com>
2021-08-13 22:04:25 -0300

    style: add glossary

---
+++ src/factories/index.js
@@ -0,0 +1,6 @@
+const makeCreateAPost = require("./create-a-post");
+const makeListAllPosts = require("./list-all-posts");
+const makeGetSpecificPost = require("./get-specific-post");
+const makeEditAParticularPost = require("./edit-a-particular-post");
+
+module.exports = { makeCreateAPost, makeListAllPosts, makeGetSpecificPost, makeEditAParticularPost };
