d76fb5f5f139ebf1d3e260aa0ab0703deeeb2af7
pedromoraisf <pedro.morais1997@gmail.com>
2021-08-13 23:00:16 -0300

    chore: ignore e2e folder

--- jest.config.js
+++ jest.config.js
@@ -5,6 +5,7 @@ module.exports = {
   rootDir: root,
   testEnvironment: "node",
   preset: "@shelf/jest-mongodb",
+  testPathIgnorePatterns: ["<rootDir>/e2e/"],
   moduleNameMapper: {
     "@/(.*)": "<rootDir>/src/$1"
   }
