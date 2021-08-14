953c15ac928febd578241c2a47993e939a5a30cf
pedromoraisf <pedro.morais1997@gmail.com>
2021-08-13 22:04:02 -0300

    feat: add edit a particular post factory

---
+++ src/factories/edit-a-particular-post.js
@@ -0,0 +1,14 @@
+const { PostRepositoryMongoDbAdapter } = require("@/adapters/database/mongodb/post-repository");
+const { routeAdapter } = require("@/adapters/http/express");
+const { editAParticularPostUseCase } = require("@/usecases");
+const { handleErrorDecorator } = require("@/usecases/decorators");
+
+const makeEditAParticularPost = async (req, res) => {
+  const { payload, ...httpResponses } = routeAdapter(req, res);
+  return await handleErrorDecorator(
+    () => editAParticularPostUseCase({ payload }, PostRepositoryMongoDbAdapter(), httpResponses),
+    httpResponses
+  );
+};
+
+module.exports = makeEditAParticularPost;
